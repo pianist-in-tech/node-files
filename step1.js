6aa82bedf824af296bf0f2b586f73fe0d0341b60
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2023-07-19 10:36:49 -0600

    added files

---
+++ step1.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+function cat(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      } else {
+        console.log(data);
+      }
+    });
+  }
+
+  cat(process.argv[2]);
+
+  
